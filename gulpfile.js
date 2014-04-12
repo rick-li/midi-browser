5b256fa23b52484fce21ab30b65fc67fa2d517c2
Rick Li <racke1983cn@gmail.com>
2014-04-12 23:59:02 +0800

    add local versions

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+var jade = require('jade');
+var qrcode = require('qrcode');
+
+var fs = require('fs');
+
+gulp.task('jade', function() {
+  var locals = JSON.parse(fs.readFileSync('./locals.js'));
+  console.log('Compiling jade.');
+  jade.renderFile('content.jade', locals.zh, function(err, html) {
+    fs.writeFileSync('zh.html', html);
+  });
+  jade.renderFile('content.jade', locals.en, function(err, html) {
+    fs.writeFileSync('en.html', html);
+  });
+});
+
+gulp.task('qr', function() {
+  qrcode.save('./files/android.png', 'http://rick-li.github.io/midi-browser/files/midi-browser1.2.5.apk');
+  qrcode.save('./files/ios.png', 'https://itunes.apple.com/cn/app/id812021388');
+});
+
+// Connect
+gulp.task('connect', $.connect.server({
+  root: ['./'],
+  port: 9000,
+  livereload: true
+}));
+
+
+// Watch
+gulp.task('watch', ['connect'], function() {
+  // Watch for changes in `app` folder
+  gulp.watch([
+    './*.html',
+  ], function(event) {
+    return gulp.src(event.path)
+      .pipe($.connect.reload());
+  });
+
+  // Watch .jade files
+  gulp.watch(['./*.jade', './locals.js'], ['jade']);
+});
